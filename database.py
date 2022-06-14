d769d7338ad054a2c415895ba799563b9c8c66b3
21Coochie <brghtmalone@gmail.com>
2022-06-14 15:16:45 +0000

    Initia cmmit of book databse using dictionary

---
+++ database.py
@@ -0,0 +1,91 @@
+from unicodedata import name
+
+#The dictionary is in the form of Book_Database=Book_ID:{Genre,Title ,Author}
+
+Book_Database ={}
+
+Book_Database['001']={'Genre': 'Action and Adventure',
+                      'Title': 'The Three Musketeers',
+                      'Author1': 'Alexander Dumas'}
+
+Book_Database['002']={'Genre': 'Graphic and Comic',
+                      'Title': 'Treasure Island',
+                      'Author': 'Robert Louis Stevenson'}
+
+Book_Database['003']={'Genre': 'Action and Adventure',
+                      'Title': 'Life of Pi',
+                      'Author': 'Yann  Martel'}
+
+Book_Database['004']={'Genre': 'Graphic and Comicr',
+                      'Title': 'Black Panther',
+                      'Author': 'Ta-Nehisi Coates and Brian Stelfreeze'}
+
+Book_Database['005']={'Genre': 'Graphic and Comic',
+                      'Title': 'Batman;The Dark Knight Returns',
+                      'Author': 'Frank Miller, Klaus Janson and Lynn Varley'}
+
+Book_Database['006']={'Genre': 'Horror',
+                      'Title': 'Bird Box',
+                      'Author': 'Josh Malerman'}
+
+Book_Database['007']={'Genre': 'Horror',
+                      'Title': 'Carrie',
+                      'Author': 'Stephen King'}
+
+Book_Database['008']={'Genre': 'Horror',
+                      'Title': 'The Haunting of Hill House',
+                      'Author': 'Shirley Jackson'}
+
+Book_Database['009']={'Genre': 'Romance',
+                      'Title': 'Brazen and The Beast ',
+                      'Author': 'Sarah MacLean'}
+
+Book_Database['0010']={'Genre': 'Romance',
+                       'Title': 'Wife by Wednesday',
+                       'Author': 'Catherine Bybee'}
+
+Book_Database['0011']={'Genre': 'Romance',
+                       'Title': 'Virgin River',
+                       'Author': 'Robyn Carr'}
+
+Book_Database['0012']={'Genre': 'Romance',
+                       'Title': 'In Bed with the Devil',
+                       'Author': 'Lorraine Heath'}
+
+Book_Database['0013']={'Genre': 'Action and Adventure',
+                       'Title': 'Mockingjay',
+                       'Author': 'Suzanne Collins'}
+
+Book_Database['0014']={'Genre': 'Action and Adventure',
+                       'Title': 'The Odessey',
+                       'Author': 'Hommer'}
+
+Book_Database['0015']={'Genre': 'Action and Adventure',
+                       'Title': 'Moby-Dick',
+                       'Author': 'Herman Melville'}
+
+Book_Database['0016']={'Genre': 'Romance',
+                       'Title': 'Mine Till Midnight',
+                       'Author': 'Lisa Kleypas'}
+
+Book_Database['0017']={'Genre': 'Graphic and Comic',
+                       'Title': 'Elf-quest',
+                       'Author': 'RWendy Pini and Richard Pini'}
+
+Book_Database['0018']={'Genre': 'Action and Adventure',
+                       'Title': 'The Adventures of Huckleberry Finn',
+                       'Author': 'Mark Twain'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
